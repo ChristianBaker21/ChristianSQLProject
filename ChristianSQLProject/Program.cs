6e6ddad8ebf899592ae33f912dd83f785d3adb92
Christian Baker <christianbaker1998@gmail.com>
2020-10-06 16:31:52 -0400

    Add project files.

---
+++ ChristianSQLProject/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using UtilityLibrayProject;
+
+namespace ChristianSQLProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var ss = new SQLServer();
+            var ok = ss.Connect("localhost", "sqlexpress", "EdDb");
+
+            var students = ss.ExcecuteQuery("SELECT * From Student;");
+
+            ss.Disconnect();
+        }
+      
+        }
+       
+        
+        }
+    
+
